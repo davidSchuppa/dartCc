3321144dc1b4d2f08ad3ededb1908d33484fe716
david <d.schuppa@gmail.com>
2019-04-03 09:46:45 +0200

    initial commit

---
+++ src/main/java/com/codecool/dartcc/DartCcApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.dartcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DartCcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DartCcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/dartcc/DartCcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codecool.dartcc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DartCcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
