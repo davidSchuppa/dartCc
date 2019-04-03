49018844b2a590584031a98e6c8f93846248eee2
david <d.schuppa@gmail.com>
2019-04-03 10:14:49 +0200

    created models: Player, Game, GameType

---
+++ src/main/java/com/codecool/dartcc/model/Player.java
@@ -0,0 +1,26 @@
+package com.codecool.dartcc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Player {
+    private String name;
+    private long id;
+    private String email;
+    private int gamesPlayed;
+    private int bestOfThree;
+    private int wins;
+    private int actualScore;
+    private int pointRemaining;
+    private double scorePerDart;
+    private double scorePerRound;
+
+
+}
