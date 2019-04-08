9036b62400c7294eacd719d752a7ff68c9addc29
david <d.schuppa@gmail.com>
2019-04-08 14:42:27 +0200

    added needed annotations for lombok and jpa

--- src/main/java/com/codecool/dartcc/model/Player.java
+++ src/main/java/com/codecool/dartcc/model/Player.java
@@ -1,25 +1,32 @@
 package com.codecool.dartcc.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@Entity
 public class Player {
-    private String name;
+    @Id
+    @GeneratedValue
     private long id;
-    private String email;
-    private int gamesPlayed;
-    private int bestOfThree;
-    private int wins;
-    private int actualScore;
-    private int pointRemaining;
-    private double scorePerDart;
-    private double scorePerRound;
+    private String name;
+//    private String email;
+//    private int gamesPlayed;
+//    private int bestOfThree;
+//    private int wins;
+//    private int actualScore;
+//    private int pointRemaining;
+//    private double scorePerDart;
+//    private double scorePerRound;
 
 
 }
