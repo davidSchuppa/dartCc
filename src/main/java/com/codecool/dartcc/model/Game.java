f1eb1b8a535adbc04b7dda06e076d6df07da2b48
david <d.schuppa@gmail.com>
2019-04-08 14:41:54 +0200

    added needed annotations for lombok and jpa

--- src/main/java/com/codecool/dartcc/model/Game.java
+++ src/main/java/com/codecool/dartcc/model/Game.java
@@ -1,19 +1,30 @@
 package com.codecool.dartcc.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Table;
 
-import java.util.ArrayList;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@Entity
+@Table(appliesTo = "game")
 public  class Game {
+    @Id
+    @GeneratedValue
+    private long id;
     private int round;
-    private ArrayList players = new ArrayList();
+    @ManyToOne
+    private Player playerOne;
+    @ManyToOne
+    private Player playerTwo;
     private int numberOfDoubles;
-    private int numberOfTriples;
-    private Player winner;
-    private GameType gameType;
+//    private int numberOfTriples;
+//    private Player winner;
+//    private GameType gameType;
 }
