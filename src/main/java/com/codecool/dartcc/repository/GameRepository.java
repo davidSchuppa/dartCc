a9e61dbf00bd1e6e824d7a4d6da8ff4b0c249b02
david <d.schuppa@gmail.com>
2019-04-09 12:22:45 +0200

    extended gamerepository with findAllByPlayerContaining method

--- src/main/java/com/codecool/dartcc/repository/GameRepository.java
+++ src/main/java/com/codecool/dartcc/repository/GameRepository.java
@@ -1,9 +1,12 @@
 package com.codecool.dartcc.repository;
 
 import com.codecool.dartcc.model.Game;
+import com.codecool.dartcc.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
 
-
+    List<Game> findAllByPlayersContaining(Player player);
 }
