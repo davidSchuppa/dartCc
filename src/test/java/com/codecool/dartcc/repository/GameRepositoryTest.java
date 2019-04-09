4485f898d1dc890260ea21497d5fdc2c71ba94bf
david <d.schuppa@gmail.com>
2019-04-09 12:23:55 +0200

    added test to findGamesByPlayer, h2 console enabled in application-test.properties

--- src/test/java/com/codecool/dartcc/repository/GameRepositoryTest.java
+++ src/test/java/com/codecool/dartcc/repository/GameRepositoryTest.java
@@ -2,6 +2,7 @@ package com.codecool.dartcc.repository;
 
 import com.codecool.dartcc.model.Game;
 import com.codecool.dartcc.model.Player;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import org.junit.runner.RunWith;
@@ -9,9 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
@@ -19,10 +21,14 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@ActiveProfiles("test")
 public class GameRepositoryTest {
 
     @Autowired
     GameRepository gameRepository;
+
+    @Autowired
+    PlayerRepository playerRepository;
 
     @Test
     public void smoke() {
@@ -42,6 +48,29 @@ public class GameRepositoryTest {
         assertThat(games).hasSize(1);
     }
 
+    @Test
+    public void testFindGamesByPlayer() {
+        Player p1 = Player.builder().name("John").build();
+        Player p2 = Player.builder().name("Jane").build();
+        Player p3 = Player.builder().name("Joe").build();
 
 
+        Game testGame = Game.builder().player(p1).player(p2).build();
+        Game testGame2 = Game.builder().player(p2).player(p3).build();
+        Game testGame3 = Game.builder().player(p2).player(p1).build();
+
+        p1.getGames().add(testGame);
+        p2.getGames().add(testGame);
+        gameRepository.saveAndFlush(testGame);
+        p3.getGames().add(testGame2);
+        p2.getGames().add(testGame2);
+        gameRepository.saveAndFlush(testGame2);
+        p1.getGames().add(testGame3);
+        p2.getGames().add(testGame3);
+        gameRepository.saveAndFlush(testGame3);
+
+        List<Game> games = gameRepository.findAllByPlayersContaining(p1);
+
+        assertThat(games.get(1)).isEqualTo(testGame3);
+    }
 }
