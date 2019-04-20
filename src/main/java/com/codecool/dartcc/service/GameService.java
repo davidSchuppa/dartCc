5c8711c30d654347e338cef3457d6f391af338a2
david <d.schuppa@gmail.com>
2019-04-20 08:29:40 +0200

    added test and todo for gameService

--- src/main/java/com/codecool/dartcc/service/GameService.java
+++ src/main/java/com/codecool/dartcc/service/GameService.java
@@ -1,4 +1,14 @@
 package com.codecool.dartcc.service;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
 public class GameService {
+
+    public long createGame() {
+        //TODO
+        //Add gameData parameter, create new game object and save to db
+        return 5;
+    }
 }
---
+++ src/test/java/com/codecool/dartcc/service/GameServiceTest.java
@@ -0,0 +1,16 @@
+package com.codecool.dartcc.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameServiceTest {
+
+    @Test
+    public void testCreateGameReturnsLastGameId() {
+
+
+    }
+
+
+}
