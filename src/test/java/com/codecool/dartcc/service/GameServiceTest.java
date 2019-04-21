0de97e1a531f4e8a59465ebc79af41f7cce3456d
david <d.schuppa@gmail.com>
2019-04-21 09:45:45 +0200

    added testCase if createGame returns last added games id

--- src/test/java/com/codecool/dartcc/service/GameServiceTest.java
+++ src/test/java/com/codecool/dartcc/service/GameServiceTest.java
@@ -1,15 +1,30 @@
 package com.codecool.dartcc.service;
 
+import com.codecool.dartcc.repository.GameRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameServiceTest {
 
+    private GameService gameService = new GameService();
+
     @Test
-    public void testCreateGameReturnsLastGameId() {
+    public void testCreateGameReturnsLastGameId() throws JsonProcessingException {
 
+        Object playerNames = new Object() {
+            public final String playerOne = "Peti";
+            public final String playerTwo = "Feri";
+        };
 
+        ObjectMapper mapper = new ObjectMapper();
+        String namesJson = mapper.writeValueAsString(playerNames);
+        long result = gameService.createGame(namesJson);
+
+        assertThat(result).isEqualTo(1);
     }
 
 
