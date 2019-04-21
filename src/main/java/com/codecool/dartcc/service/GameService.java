ef67ea6fcf455bc5faca934eaca1613b90109c2b
david <d.schuppa@gmail.com>
2019-04-21 09:44:11 +0200

    create game receives playerNames as parameters

--- src/main/java/com/codecool/dartcc/service/GameService.java
+++ src/main/java/com/codecool/dartcc/service/GameService.java
@@ -1,14 +1,20 @@
 package com.codecool.dartcc.service;
 
+import com.codecool.dartcc.repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Component
 public class GameService {
 
-    public long createGame() {
+    @Autowired
+    GameRepository gameRepository;
+
+
+    public long createGame(String playerNames) {
         //TODO
         //Add gameData parameter, create new game object and save to db
-        return 5;
+        return 0;
     }
 }
