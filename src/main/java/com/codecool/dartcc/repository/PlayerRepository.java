1569ab491c16b1817325688a9959e25d5ec49747
david <d.schuppa@gmail.com>
2019-04-24 11:34:14 +0200

    added findPlayerByName method

--- src/main/java/com/codecool/dartcc/repository/PlayerRepository.java
+++ src/main/java/com/codecool/dartcc/repository/PlayerRepository.java
@@ -2,7 +2,10 @@ package com.codecool.dartcc.repository;
 
 import com.codecool.dartcc.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
+    Player findPlayerByName(String name);
 }
