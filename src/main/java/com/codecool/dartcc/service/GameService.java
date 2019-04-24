4fc5df44c72b94fef2ae9c1bcf9ff89bfabf9fc4
david <d.schuppa@gmail.com>
2019-04-24 10:40:31 +0200

    implemented createGame method, covered with test

--- src/main/java/com/codecool/dartcc/service/GameService.java
+++ src/main/java/com/codecool/dartcc/service/GameService.java
@@ -1,11 +1,15 @@
 package com.codecool.dartcc.service;
 
+import com.codecool.dartcc.model.Game;
+import com.codecool.dartcc.model.Player;
 import com.codecool.dartcc.repository.GameRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Component
+import java.io.IOException;
+
+@Service
 public class GameService {
 
     @Autowired
@@ -13,8 +17,21 @@ public class GameService {
 
 
     public long createGame(String playerNames) {
-        //TODO
-        //Add gameData parameter, create new game object and save to db
-        return 0;
+        long gameId = 0;
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String playerOne = String.valueOf(mapper.readTree(playerNames).get("playerOne"));
+            String playerTwo = String.valueOf(mapper.readTree(playerNames).get("playerTwo"));
+
+            Player p1 = Player.builder().name(playerOne).build();
+            Player p2 = Player.builder().name(playerTwo).build();
+
+            Game newGame = Game.builder().p1(p1).p2(p2).build();
+            gameRepository.saveAndFlush(newGame);
+            gameId = newGame.getId();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return gameId;
     }
 }
