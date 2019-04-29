e782e56dace0452c4363132ba9cd2ae1b715d1ea
david <d.schuppa@gmail.com>
2019-04-29 13:23:13 +0200

    added custom GameNotFoundException

--- src/main/java/com/codecool/dartcc/exception/GameNotFoundException.java
+++ src/main/java/com/codecool/dartcc/exception/GameNotFoundException.java
@@ -1,4 +1,23 @@
 package com.codecool.dartcc.exception;
 
 public class GameNotFoundException extends Throwable {
+
+    public GameNotFoundException() {
+    }
+
+    public GameNotFoundException(String message) {
+        super(message);
+    }
+
+    public GameNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GameNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public GameNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
